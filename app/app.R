4916c6c8c75b382b62f8743cd5df669604394702
ram <ramgoennka08@gmail.com>
2024-03-07 03:01:48 -0600

    skeleton of the application developed

--- app/app.R
+++ app/app.R
@@ -1,51 +1,53 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+#app.R
 library(shiny)
+library(shinydashboard)
+#TODO 1: Need to create individual RShiny files for each of the three phases 
+#TODO 2: Connect the RShiny files for each of the three phases to the main file (app.R)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+#UI
+ui <- dashboardPage(
+  dashboardHeader(title = "3 Phase"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Phase 1", tabName = "phase1"),
+      menuItem("Phase 2", tabName = "phase2"),
+      menuItem("Phase 3", tabName = "phase3")
     )
+  ),
+  dashboardBody(
+    tabItems(
+      #HOME PAGE
+      tabItem(tabName = "home",
+              h2("3 Phase application (change name/title later)"),
+              div(
+                h3("Purpose"),
+                p("Purpose of the application will go here..."),
+                h3("Usage"),
+                p("A brief outline of how to use the application will go here...")
+              )
+      ),
+      #PHASE 1
+      tabItem(tabName = "phase1",
+              h2("Phase 1"),
+              p("Content for Phase 1...")
+      ),
+      #PHASE 2
+      tabItem(tabName = "phase2",
+              h2("Phase 2"),
+              p("Content for Phase 2...")
+      ),
+      #PHASE 3
+      tabItem(tabName = "phase3",
+              h2("Phase 3"),
+              p("Content for Phase 3...")
+      )
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+#SERVER LOGIC
+server <- function(input, output) { }
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
-
-# Run the application 
 shinyApp(ui = ui, server = server)
+
